5c5c78201033dc3969ebf238d37aba8df8c4fe7e
Zheng5005 <u.gomezherrera@gmail.com>
2025-11-04 12:27:31 -0600

    Register JS, needs testing

--- backend/run.bat
+++ backend/run.bat
@@ -1,26 +1,12 @@
-@echo off
-for /f "tokens=2 delims==" %%i in ('wmic os get caption /value 2^>nul') do set OSNAME=%%i
-echo Sistema detectado: %OSNAME%
+# crear entorno virtual
+#python -m venv env # Windows
+python3 -m venv env # Linux
 
-if /i "%OSNAME%"=="Microsoft Windows" (
-    echo Ejecutando en Windows...
-    if not exist env (
-        python -m venv env
-    )
-    call env\Scripts\activate
-) else (
-    echo Ejecutando en Linux/WSL...
-    if not exist env (
-        python3 -m venv env
-    )
-    call env/bin/activate
-)
+# activar entorno virtual
+#source env/Scripts/activate # Windows
+source env/bin/activate # Linux
 
-if exist requirements.txt (
-    echo Instalando dependencias...
-    pip install -r requirements.txt
-)
-
-echo Iniciando servidor...
+# instalar dependencias
+pip install -r requirements.txt
+# ejecutar la aplicación
 uvicorn main:app --reload
-pause
