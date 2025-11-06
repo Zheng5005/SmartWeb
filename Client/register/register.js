e5c9b3d24d65d02a1716ced98d0ee4795a8d8bfd
Gungrave <jorgefranciscocz@gmail.com>
2025-11-05 20:47:31 -0600

    Update register.js

--- Client/register/register.js
+++ Client/register/register.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (userType === "teacher") {
       motivation = document.getElementById("motivation").value.trim();
       if (!motivation) {
-       alert("Por favor, explique su motivación para ser profesor.");
+        alert("Por favor, explique su motivación para ser profesor.");
         return;
       }
     }
